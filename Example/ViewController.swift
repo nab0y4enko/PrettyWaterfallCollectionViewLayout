d313eeb18dafd106a845e8aa676294bfc4df1b0d
Oleksii Naboichenko <naboichenko@zfort.com>
2016-12-18 23:06:18 +0200

    Updated example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,53 +9,66 @@
 import UIKit
 import PrettyWaterfallCollectionViewLayout
 
-fileprivate typealias SizeAndColor = (size: CGSize, color: UIColor)
+class Item {
+    // MARK: - Public Properties
+    let size: CGSize
+    let color: UIColor
+    
+    init() {
+        size = CGSize.random(lower: 120, upper: 600)
+        color = UIColor.random()
+    }
+}
+
+class Section {
+    // MARK: - Public Properties
+    var numberOfColumns: Int = Int(UInt32.random(lower: 1, upper: 5))
+    var headerColor: UIColor = UIColor.random()
+    var items: [Item] = Array(repeating: Item(), count: 30)
+    var footerColor: UIColor = UIColor.random()
+}
 
 class ViewController: UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - Private Properties
-    fileprivate lazy var data: [SizeAndColor] = {
-        var data: [SizeAndColor] = []
-        (0..<100).forEach({ (_) in
-            let randomSize = CGSize.random(lower: 120, upper: 600)
-            let randomColor = UIColor.random()
-            let sizeAndColor = SizeAndColor(size: randomSize, color: randomColor)
-            data.append(sizeAndColor)
-        })
-        return data
-    }()
-    
-    // MARK: - UIViewController
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    fileprivate lazy var dataSource: [Section] = Array(repeating: Section(), count: 10)
 }
 
 // MARK: - UICollectionViewDataSource
 extension ViewController: UICollectionViewDataSource {
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return dataSource.count
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.count
+        return dataSource[section].items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ExampleCell", for: indexPath) as! ColoredCell
-        cell.backgroundColor = data[indexPath.row].color
+        cell.backgroundColor = dataSource[indexPath.section].items[indexPath.row].color
         cell.textLabel.text = String(indexPath.item)
         return cell
     }
 }
 
+// MARK: - PrettyWaterfallCollectionViewLayoutDelegate
 extension ViewController: PrettyWaterfallCollectionViewLayoutDelegate {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, numberOfColumnsInSection section: Int) -> Int {
-        return 3
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForItemAt indexPath: IndexPath) -> CGSize {
+        return dataSource[indexPath.section].items[indexPath.row].size
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForItemAt indexPath: IndexPath) -> CGSize {
-        return data[indexPath.row].size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, numberOfColumnsInSection section: Int) -> Int {
+        return dataSource[section].numberOfColumns
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumColumnSpacingForSectionAt section: Int) -> CGFloat {
+        return 20.0
     }
 
 }
